8eccebe60253548b9d445be572b555190bcc0527
Blake McLachlin <blake.mclachlin@icloud.com>
2020-05-22 23:34:13 -0600

    Started working on board

---
+++ v1/js/scripts.js
@@ -0,0 +1,22 @@
+let board = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]
+];
+
+// function updateBoard() {
+//     let cellID = 0;
+//     let prefix = "cell-";
+//     for (let i = 0; i < 9; i++) {
+//         for (let j = 0; j < 9; j++) {
+//             let selector = prefix + cellID.toString();
+//             document.getElementById(selector);
+//         }
+//     }
+// }
