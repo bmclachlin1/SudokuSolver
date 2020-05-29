e438da8bdf6f7bab74b8c8473d781bd7a5e819f7
Blake McLachlin <blake.mclachlin@icloud.com>
2020-05-28 23:02:49 -0600

    Tried implementing update board function for whenever a user enters something in an html input element. Not working right now. Also, would like to have it not loop through the whole board to know which cell to update

--- v1/js/scripts.js
+++ v1/js/scripts.js
@@ -1,3 +1,6 @@
+/**
+ * Our sudoku board. 
+ */
 let board = [
     [0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0],
@@ -10,13 +13,37 @@ let board = [
     [0,0,0,0,0,0,0,0,0]
 ];
 
-// function updateBoard() {
-//     let cellID = 0;
-//     let prefix = "cell-";
-//     for (let i = 0; i < 9; i++) {
-//         for (let j = 0; j < 9; j++) {
-//             let selector = prefix + cellID.toString();
-//             document.getElementById(selector);
-//         }
-//     }
-// }
+/* select all input tags, needed for event listener */
+let inputList = document.querySelectorAll('input');
+
+/* add event listener to all input elements */
+inputList.forEach(input => {
+    input.addEventListener('input', updateBoard);
+});
+
+/** 
+ *   Update our board array when a user enters a value in our board 
+ */
+function updateBoard() {
+    let cellID = 0;
+    let prefix = "cell-";
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            // get id of cell we are updating
+            let selector = prefix + cellID.toString();
+            console.log(selector);
+            // select the cell
+            let cell = document.getElementById(selector);
+            console.log(cell);
+            // get the text content of the cell
+            let cellContent = Number(cell.textContent);
+            console.log(cellContent);
+            // update board position 
+            board[i][j] = cellContent;
+            // increment cell ID and move to next row/column
+            cellID++;
+        }
+    }
+    // for development purposes
+    console.table(board);
+}
